df2637878d18c01fc83282e4e3f0bca05a790120
nueng-ct <phisit.yu@gmail.com>
2018-04-09 13:39:14 +0700

    revision-37

--- __openerp__.py
+++ __openerp__.py
@@ -7,7 +7,7 @@
     'depends':['account','product','stock','purchase','sale_service','project_timesheet','mrp_repair','thaireefer_extended','report_xlsx','sale','thai_accounting','thai_accounting_taxinvoice'],
     'website':'http://www.thaireefer.co.th',
     'data':['report/report_reg.xml',
-            'report/invoice_trf_report100_id.xml',
+            'report/invoice_trf_report110_id.xml',
             'report/quotation_trf_report101_id.xml',
             'report/taxinvoice_trf_report102_id.xml',
             'report/invoice_group_report_103_id.xml',
