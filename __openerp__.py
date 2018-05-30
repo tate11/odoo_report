d62ee92081da718e5d81870179e1229c4425adfb
nueng-ct <phisit.yu@gmail.com>
2018-05-30 08:21:50 +0700

    Revision-71
    Update  Invoice Agent One

--- __openerp__.py
+++ __openerp__.py
@@ -27,7 +27,8 @@
             'report/vendorvoucher_report_119_id.xml',
             'report/invoice_mahachai_report_121_id.xml',
             'report/generalvoucher_report_122_id.xml',
-            'report/quotation_std_report123_id.xml'],
+            'report/quotation_std_report123_id.xml',
+            'report/invoice_one_report124_id.xml'],
     'auto_install':False,
     'application':True
 }
